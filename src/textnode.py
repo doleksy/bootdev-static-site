9e7250a754f2fa2c68547699aa76eae393df983f
Dave Oleksy <dave@doleksy.com>
2025-08-08 15:43:03 -0500

    Fixed error (forgot to add self to class methods)

--- src/textnode.py
+++ src/textnode.py
@@ -9,13 +9,13 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode:
-    def __init__(text, text_type, url=None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
-    def __eq__(o):
+    def __eq__(self, o):
         return (self.text == o.text) and (self.text_type == o.text_type) and (self.url == o.url)
-    def __repr__():
+    def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 def test():
