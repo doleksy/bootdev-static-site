7d24512ecf973624514ceb43265b8e0f9414e0bb
Dave Oleksy <dave@doleksy.com>
2025-08-12 15:44:46 -0500

    add markdown to blocks

---
+++ src/markdown_to_blocks.py
@@ -0,0 +1,25 @@
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
+
+def test():
+    markdown =  """
+# This is a heading
+
+This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
+
+- This is the first list item in a list block
+- This is a list item
+- This is another list item
+    """
+    blocks = markdown_to_blocks(markdown)
+    print(blocks)
+
+if __name__ == "__main__":
+    test()
