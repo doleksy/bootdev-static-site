ce19a97db48e43abe55f278476756fa8f1b25955
Dave Oleksy <dave@doleksy.com>
2025-08-18 14:42:47 -0500

    Copied solution from boot.dev

--- src/markdown.py
+++ src/markdown.py
@@ -5,29 +5,102 @@ from textnode import TextNode, TextType
 from text_to_textnode import text_to_textnodes
 
 
-
-
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
-
-    html_nodes = []
+    children = []
     for block in blocks:
-        print(f"block: {block}")
-        block_type = block_to_block_type(block)
-        print(f"Block type: {block_type.value}")
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
 
-        if block_type == BlockType.PARAGRAPH:
-            child_nodes = text_to_children(block)
 
-            parent = ParentNode("p", block)
-            html_nodes.append(parent)
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    if block_type == BlockType.PARAGRAPH:
+        return paragraph_to_html_node(block)
+    if block_type == BlockType.HEADING:
+        return heading_to_html_node(block)
+    if block_type == BlockType.CODE:
+        return code_to_html_node(block)
+    if block_type == BlockType.ORDERED_LIST:
+        return olist_to_html_node(block)
+    if block_type == BlockType.UNORDERED_LIST:
+        return ulist_to_html_node(block)
+    if block_type == BlockType.QUOTE:
+        return quote_to_html_node(block)
+    raise ValueError("invalid block type")
 
-    return ParentNode("div", html_nodes)
 
 def text_to_children(text):
     text_nodes = text_to_textnodes(text)
-    print(f"Text nodes: {text_nodes}")
-    return [text_node_to_html_node(node) for node in text_nodes]
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+
+def heading_to_html_node(block):
+    level = 0
+    for char in block:
+        if char == "#":
+            level += 1
+        else:
+            break
+    if level + 1 >= len(block):
+        raise ValueError(f"invalid heading level: {level}")
+    text = block[level + 1 :]
+    children = text_to_children(text)
+    return ParentNode(f"h{level}", children)
+
+
+def code_to_html_node(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("invalid code block")
+    text = block[4:-3]
+    raw_text_node = TextNode(text, TextType.TEXT)
+    child = text_node_to_html_node(raw_text_node)
+    code = ParentNode("code", [child])
+    return ParentNode("pre", [code])
+
+
+def olist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
+
+
+def ulist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
+
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
 
 def test_paragraphs():
     md = """
@@ -40,9 +113,10 @@ This is another paragraph with _italic_ text and `code` here
 """
 
     node = markdown_to_html_node(md)
-    print(node)
+    print(f"TEST node: {node}")
     html = node.to_html()
-    print(html)
+    print(f"TEST html: {html}")
+# "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>"
 
 def test_codeblock():
     md = """
@@ -56,7 +130,7 @@ the **same** even with inline stuff
     print(node)
     html = node.to_html()
     print(html)
-
+# "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>"
 if __name__ == "__main__":
     test_paragraphs()
     #test_codeblock()
