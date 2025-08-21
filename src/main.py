da81680c44c1c0b745e4b57b6dfa245becb17f63
Dave Oleksy <dave@doleksy.com>
2025-08-20 19:23:26 -0500

    Update main/py and main.sh to launch webserver

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+from generate import generate_page
 from textnode import TextNode, TextType
 
 def main():
@@ -12,6 +13,11 @@ def copy_static():
 
     shutil.rmtree("public/", ignore_errors=True)
     shutil.copytree("static/", "public/")
+
+    from_path = "./content/index.md"
+    template_path = "./template.html"
+    dest_path = "./public/index.html"
+    generate_page(from_path, template_path, dest_path)
 
 if __name__ == "__main__":
     main()
